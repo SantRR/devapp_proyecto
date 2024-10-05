c407e94e863ed72655c2ed4bd9ae922858f32d75
SantRR <142363102+SantRR@users.noreply.github.com>
2024-10-05 00:56:33 -0600

    Third commit
    Nueva estructura para main.py

--- Model/main.py
+++ Model/main.py
@@ -1,17 +1,9 @@
-from Model.conexion import Conexion
-from Model.producto_base import ProductoBase
+from Model.load_login import LoadLogin
+from PyQt6.QtWidgets import QApplication
 import sys
 
-def main():
-    producto_base = ProductoBase()
-    """
-    producto_base.producto.clave       = "SKU-31"
-    producto_base.producto.descripcion = "Papitas" 
-    producto_base.producto.existencia  = 10
-    producto_base.producto.precio      = 32
-    """
-    producto_base.contar_producto()
-    return
-    
 if __name__ == "__main__":
-    main()
+    app = QApplication(sys.argv)
+    ventana = LoadLogin()
+    ventana.show()
+    sys.exit(app.exec())
